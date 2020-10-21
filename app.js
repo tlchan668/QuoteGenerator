ecbc8cc597c388acfec091d1a2a5961afd2edd64
Traci Chan <tlc668@gmail.com>
2020-10-21 14:13:17 -0400

    added quotes

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ let quotes = [
     '"If life were predictable it would cease to be life, and be without flavor", Eleanor Roosevelt',
     '"When you reach the end of your rope, tie a knot in it and hang on.", Franklin D. Roosevelt',
     '"Whoever is happy will make others happy too.",Anne Frank',
+    '"I planned out our whole day.  First, we will make snow angels for two hours.  Then we will go ice skating.  then we will eat a whole roll of cookie dough, and then we will snuggle.", ELF',
+    '"There is a certain magic that comes with the very fist snow. For when the first snow is also a Christmas snow, well, Something wonderful is bound to happen.", Frost the Snowman',
+    '"I will honor Christmas in my heart and try to keep it all the year.", A Christmas Carol',
+    '"The world is what you make it.  And it all starts with what you make of yourself." Fred Claus',
+    '"Just remember the true spirit of Christmas lies in your heart.", The Polar Express',
     
 ];
 //create the function to display it
